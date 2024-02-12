b42b0d0d65cbdc12508ccfe913784b07e202adee
ashutoshtiwari11 <115335561+ashutoshtiwari11@users.noreply.github.com>
2024-02-12 16:45:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
+
+# Replace 'number' with the actual phone number you want to check
+number = "+919149365604"
+
+pepnumber = phonenumbers.parse(number, None)
+location = geocoder.description_for_number(pepnumber, "en")
+print("Location:", location)
+
+service_pro = phonenumbers.parse(number, "en")
+print("Carrier:", carrier.name_for_number(service_pro, "en"))
+print(timezone.time_zones_for_geographical_number(service_pro))
+
+#installed open cage as third party library for exact locatuon
+from opencage.geocoder import OpenCageGeocode
+
+key = "682aef1b8b0a4bdc98ca050fb4f0ea50"
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results= geocoder.geocode(query)
+print(results)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+
+print(lat, lng)
+
+import folium 
+
+mymap = folium.Map(location=[lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=location).add_to(mymap)
+
+mymap.save("mylocation.html") 
